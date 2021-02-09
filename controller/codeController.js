af38b7ea46cee602cf9d53a16988191cb3e0fee1
Har-SHAW <8888harsha4444@gmail.com>
2021-02-09 14:44:32 +0530

    init

--- controller/codeController.js
+++ controller/codeController.js
@@ -1,7 +1,7 @@
 const { exec, execFile } = require('child_process');
 fs = require('fs');
 
-const compiler = "g++";
+const compiler = "gcc";
 const version = "-std=c++11";
 const out = "-o";
 const infile = "code-runner.cpp";
